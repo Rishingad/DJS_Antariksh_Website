e0db4932ac32ddb44db45660e2fac74a87150e4d
KreenaShah <102236893+KreenaShah@users.noreply.github.com>
2023-01-28 08:11:30 +0530

    achievement , department component done

--- djsa_website/src/components/Achievement/Achievement.js
+++ djsa_website/src/components/Achievement/Achievement.js
@@ -1,10 +1,112 @@
 import React from 'react'
+import './Achievement.css'
+// import Antariksh_HomePageS2 from './Antariksh_HomePageS2.jpg';
+// import AntarikshLogo_Zoom from './AntarikshLogo_Zoom.png'
+import science_award from './science_award.jpg'
+import navigation_award from './navigation_award.jpg'
+import back from './back.png'
+
+import { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import "./styles.css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
 
 function Achievement() {
   return (
     <div>
-      <div className='Home_container'>
+      <div className='Achievement_container'>
         <div className='star'></div>
+
+        <Swiper
+          sx={{
+            width: 700
+          }}
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 4500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+        <div className='skew'>
+          <div className='skew_img'>
+            <img src={science_award}></img>
+          </div>
+          <div className='skew_content_container'>
+            <div className='skew_content'>
+              <p className='heading'>European Rover Challenge 2022</p>
+              <div className='text'>
+                <div className='text1'>
+                  <p className='sub-text1'>~ 1st Position Worldwide - Remote Edition</p>
+                </div>
+                <div className='text1'>
+                  <p className='sub-text2-1'>~ 1st Asian Team to be the</p>
+                  <p className='champ'>CHAMPIONS OF ERC</p>
+                  <p className='sub-text2-2'>for two consecutive years</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className='skew'>
+          <div className='skew_img'>
+            <img src={navigation_award}></img>
+          </div>
+          <div className='skew_content_container'>
+            <div className='skew_content'>
+              <p className='heading'>European Rover Challenge 2021</p>
+              <div className='text'>
+                <div className='text1'>
+                  <p className='sub-text1'>~ 1st Position Worldwide - Remote Edition</p>
+                </div>
+                <div className='text1'>
+                  <p className='sub-text1'>~ BEST TEAM : <p className='sci'>SCIENCE && NAVIGATION</p></p>
+                  
+                  {/* <p className='nav'>NAVIGATION</p> */}
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+        {/* <div className='skew'>
+          <div className='skew_img'>
+            <img src={science_award}></img>
+          </div>
+          <div className='skew_content_container'>
+            <div className='skew_content'>
+              <p className='heading'>European Rover Challenge 2022</p>
+              <div className='text'>
+                <div className='text1'>
+                  <p className='sub-text1'>~ 1st Position Worldwide - Remote Edition</p>
+                </div>
+                <div className='text1'>
+                  <p className='sub-text2-1'>~ 1st Asian Team to be the</p>
+                  <p className='champ'>CHAMPIONS OF ERC</p>
+                  <p className='sub-text2-2'>for two consecutive years</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> */}
+
       </div>
     </div>
   )
--- djsa_website/src/components/Department/Department.js
+++ djsa_website/src/components/Department/Department.js
@@ -1,13 +1,68 @@
-import React from 'react'
+import React from "react";
+// import XYZ from "./XYZ";
+import Marquee from "react-fast-marquee";
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+// import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+import Antariksh_HomePageS2 from "./Antariksh_HomePageS2.jpg";
+import "./Department.css";
+
+import coding from "../Department/coding.png";
+import elex from "../Department/elex.png";
+import marketing from "../Department/marketing.png";
+import mech from "../Department/mech.png";
+import sci from '../Department/sci.png';
 
 function Department() {
   return (
     <div>
-      <div className='Home_container'>
-        <div className='star'></div>
+      <div className="Dept_container">
+        <div className="Dept_star"></div>
+        <div>
+          <Marquee
+            gradient={false}
+            speed={80}
+            pauseOnHover={true}
+            pauseOnClick={true}
+            delay={0}
+            play={true}
+            direction="left"
+          >
+            <Card className="skill--box">
+              <CardContent>
+                <img className="image" src={coding} height={500} width={700} />
+              </CardContent>
+            </Card>
+
+            <Card className="skill--box">
+              <CardContent>
+                <img className="image" src={sci} height={500} width={700} />
+              </CardContent>
+            </Card>
+            <Card className="skill--box">
+              <CardContent>
+                <img className="image" src={elex} height={500} width={700} />
+              </CardContent>
+            </Card>
+            <Card className="skill--box">
+              <CardContent>
+                <img className="image" src={marketing} height={500} width={700} />
+              </CardContent>
+            </Card>
+            <Card className="skill--box">
+              <CardContent>
+                <img className="image" src={mech} height={500} width={700} />
+              </CardContent>
+            </Card>
+          </Marquee>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Department;
